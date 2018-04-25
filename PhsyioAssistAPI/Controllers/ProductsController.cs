6f599b2786f8ab7d2f36a293fd4c4d16207358b8
Charmeline Tiel <c.tiel@hotmail.com>
2018-04-25 23:09:16 +0200

    init

---
+++ PhsyioAssistAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using PhsyioAssistAPI.Models;
+
+namespace PhsyioAssistAPI.Controllers
+{
+    [EnableCors(origins: "http://localhost:4400", headers: "*", methods: "*")]
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Client 1"},
+            new Product { Id = 2, Name = "Client 2"},
+            new Product { Id = 3, Name = "Client 3"}
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
